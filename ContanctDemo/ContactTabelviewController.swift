8ef62d161f452d825ffeb5ae17e5c8a49b51a719
欧阳云慧 <ouyangyunhui@promote.cache-dns.local>
2015-12-02 10:56:37 +0800

    seal and package

--- ContanctDemo/ContactTabelviewController.swift
+++ ContanctDemo/ContactTabelviewController.swift
@@ -238,7 +238,8 @@ class ContactTabelviewController: UITableViewController,CNContactPickerDelegate,
                     let index = topIndex.startIndex.advancedBy(1)
                     var firstLetter: String = topIndex.substringToIndex(index)
                     firstLetter = firstLetter.uppercaseString
-                    firstLetter = polyphonicCharacter(contact, first: firstLetter)
+                    firstLetter = FirstWord.polyphonicCharacter(contact, first: firstLetter)
+                    //firstLetter = polyphonicCharacter(contact, first: firstLetter)
                     if rowArray.isEmpty{
                         rowArray.append(StringOrCNContact.StringValue(firstLetter))
                         rowArray.append(StringOrCNContact.CNContactValue(contact))
@@ -264,69 +265,7 @@ class ContactTabelviewController: UITableViewController,CNContactPickerDelegate,
         return indexContacter
     }
     
-//MARK: -- polyphonic character
-    func polyphonicCharacter(contact: CNContact, var first: String) -> String {
-        switch contact.familyName {
-        case "沈" :
-            first = "S"
-        case "乐":
-            first = "Y"
-        case "种":
-            first = "Z"
-        case "单":
-            first = "S"
-        case "解":
-            first = "X"
-        case "查":
-            first = "Z"
-        case "曾":
-            first = "Z"
-        case "区":
-            first = "O"
-        case "繁":
-            first = "P"
-        case "仇":
-            first = "C"
-        case "幺":
-            first = "Y"
-        case "折":
-            first = "S"
-        case "晁":
-            first = "C"
-        case "贾":
-            first = "J"
-        case "馮":
-            first = "F"
-        case "哈":
-            first = "H"
-        case "尾":
-            first = "W"
-        case "盛":
-            first = "S"
-        case "塔":
-            first = "T"
-        case "谷":
-            first = "G"
-        case "乾":
-            first = "Q"
-        case "乜":
-            first = "N"
-        case "陶":
-            first = "T"
-        case "阚":
-            first = "K"
-        case "叶":
-            first = "Y"
-        case "车":
-            first = "C"
-        case "艾":
-            first = "A"
-        default: break
-        }
-        return first
-    }
-
-    
+   
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
---
+++ ContanctDemo/firstCharacter.swift
@@ -0,0 +1,74 @@
+//
+//  firstCharacter.swift
+//  ContanctDemo
+//
+//  Created by 欧阳云慧 on 15/12/2.
+//  Copyright © 2015年 欧阳云慧. All rights reserved.
+//
+import UIKit
+import Foundation
+import Contacts
+
+class FirstWord {
+    static func polyphonicCharacter(contact: CNContact, first: String) -> String {
+        var first = first
+        switch contact.familyName {
+        case "沈" :
+            first = "S"
+        case "乐":
+            first = "Y"
+        case "种":
+            first = "Z"
+        case "单":
+            first = "S"
+        case "解":
+            first = "X"
+        case "查":
+            first = "Z"
+        case "曾":
+            first = "Z"
+        case "区":
+            first = "O"
+        case "繁":
+            first = "P"
+        case "仇":
+            first = "C"
+        case "幺":
+            first = "Y"
+        case "折":
+            first = "S"
+        case "晁":
+            first = "C"
+        case "贾":
+            first = "J"
+        case "馮":
+            first = "F"
+        case "哈":
+            first = "H"
+        case "尾":
+            first = "W"
+        case "盛":
+            first = "S"
+        case "塔":
+            first = "T"
+        case "谷":
+            first = "G"
+        case "乾":
+            first = "Q"
+        case "乜":
+            first = "N"
+        case "陶":
+            first = "T"
+        case "阚":
+            first = "K"
+        case "叶":
+            first = "Y"
+        case "车":
+            first = "C"
+        case "艾":
+            first = "A"
+        default: break
+        }
+        return first
+    }
+}
